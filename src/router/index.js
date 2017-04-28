9a0b5bc6059e65c9a5ab27a334072c1663607c76
Matthew Secrist <matthew.secrist@outlook.com>
2017-04-28 01:22:08 -0500

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../components/Home';
+import About from '../components/About';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+  ],
+});
