f451d9d17cd2f7334fc396776d7df1da36382121
Matthew Secrist <matthew.secrist@outlook.com>
2017-04-29 17:49:38 -0500

    Styling shows detail page

--- src/containers/ShowDetail/ShowDetail.component.js
+++ src/containers/ShowDetail/ShowDetail.component.js
@@ -1,25 +1,31 @@
 import axios from 'axios';
-import { formatDetailRequest } from '../../utilities';
+import { formatDetailRequest, formatRelatedRequest } from '../../utilities';
+import SourceLink from '../../components/SourceLink';
 
 export default {
   name: 'show-detail',
   props: [],
+  components: {
+    'source-link': SourceLink,
+  },
   mounted() {
     this.getShowDetails();
   },
   data() {
     return {
       showData: {},
+      relatedShows: [],
     };
   },
   methods: {
     getShowDetails() {
       const url = formatDetailRequest('shows', this.$route.params.id);
-      axios.get(url)
-        .then((res) => {
-          console.log(res.data)
-          this.showData = res.data;
-        });
+      const related = formatRelatedRequest('shows', this.$route.params.id);
+      axios.all([axios.get(url), axios.get(related)])
+        .then(axios.spread((show, relatedShows) => {
+          this.relatedShows = relatedShows.data.results;
+          this.showData = show.data;
+        }));
     },
   },
 };
