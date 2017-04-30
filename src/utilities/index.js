263d344c14a362ed4bc86233371439d944c3c19e
Matthew Secrist <matthew.secrist@outlook.com>
2017-04-29 19:10:05 -0500

    Update to remove api key from being hardcoded

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,8 +1,6 @@
 const API_URL = 'https://api-public.guidebox.com/v2/';
-const API_KEY = '515ce266b75ac765a709d1cede86ace60bb4be3f';
+const API_KEY = process.env.API_KEY;
 
-// Used to generate a URL, ending up being:
-// 'http://api-public.guidebox.com/v2/movies?api_key=515ce266b75ac765a709d1cede86ace60bb4be3f&limit=100'
 export function formatRequest(type, params) {
   if (params) {
     return `${API_URL}${type}?api_key=${API_KEY}&sources=${params}&limit=100`;
