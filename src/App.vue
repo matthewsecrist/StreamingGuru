8984466a95e4238501555f829cdc9664cb1ad5e2
Matthew Secrist <matthew.secrist@outlook.com>
2017-04-29 21:31:47 -0500

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <navbar></navbar>
       <router-view :key="$route.path"></router-view>
+    <footerbar></footerbar>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 export default {
   name: 'app',
   components: {
     navbar: Navbar,
+    footerbar: Footer,
   },
 };
 </script>
